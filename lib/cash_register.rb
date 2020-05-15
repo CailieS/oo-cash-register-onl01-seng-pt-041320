80077d2f50b95d53908731f5271a34f63c5e356d
Cailie Skelton <cskelton707@gmail.com>
2020-05-15 21:39:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,15 +34,3 @@ class CashRegister
     self.total = self.total - @last_item
   end 
 end 
-
-list = CashRegister.new(20) 
-list.add_item("eggs", 1.50, 3) => 4.50
-list.add_item("skirt steak", 7.99) => 7.99 
-list.add_item("apple pie", 3.99, 2) => 7.98
-list.total => 20.47
-list.items => ["eggs", "eggs", "eggs", "skirt steak", "apple pie", "apple pie"]
-list.apply_discount => "After the discount, the total comes to $16."
-list.void_last_transaction
-list.items  => ["eggs", "eggs", "eggs", "skirt steak", "apple pie"]
-list.total => 8.02
-list 
